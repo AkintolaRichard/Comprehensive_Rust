27fe51d993c0ca37a2d339abe870af43ab43497e
AkintolaRichard <princerichard17a@gmail.com>
2025-01-09 23:21:54 +0100

    chore: Add file containing borrowing a value concepts

---
+++ Day_03/afternoon/borrowing_a_value.rs
@@ -0,0 +1,41 @@
+/*
+Borrowing a Value
+As we saw before, instead of transferring ownership when calling a function,
+you can let a function borrow the value.
+*/
+
+/*#[derive(Debug)]
+struct Point(i32, i32);
+
+fn add(p1: &Point, p2: &Point) -> Point {
+    Point(p1.0 + p2.0, p1.1 + p2.1)
+}
+
+fn main() {
+    let p1 = Point(3, 4);
+    let p2 = Point(10, 20);
+    let p3 = add(&p1, &p2);
+    println!("{p1:?} + {p2:?} = {p3:?}");
+}*/
+
+/*
+ * The add function borrows two points and returns a new point.
+ * The caller retains ownership of the inputs.
+*/
+
+#[derive(Debug)]
+struct Point(i32, i32);
+
+fn add(p1: &Point, p2: &Point) -> Point {
+    let p = Point(p1.0 + p2.0, p1.1 + p2.1);
+    println!("&p.0: {:p}", &p.0);
+    p
+}
+
+pub fn main() {
+    let p1 = Point(3, 4);
+    let p2 = Point(10, 20);
+    let p3 = add(&p1, &p2);
+    println!("&p3.0: {:p}", &p3.0);
+    println!("{p1:?} + {p2:?} = {p3:?}");
+}
